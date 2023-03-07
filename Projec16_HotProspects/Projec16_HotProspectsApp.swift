a8523bd3f07660f922bfc3796197ae1607ced759
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2023-03-07 13:05:49 +0000

    Initial Commit

---
+++ Projec16_HotProspects/Projec16_HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projec16_HotProspectsApp.swift
+//  Projec16_HotProspects
+//
+//  Created by admin on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Projec16_HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
